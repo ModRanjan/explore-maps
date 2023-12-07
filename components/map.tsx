abf89e5ac65c64bb68ab68bef0b26ac16b67814c
mod ranjan <modranjan1999@gmail.com>
2023-12-07 07:35:22 +0530

    added google MarkerClusterer &

--- components/map.tsx
+++ components/map.tsx
@@ -15,6 +15,7 @@ type MapOptions = google.maps.MapOptions;
 
 export default function Map() {
   const [office, setOffice] = useState<LatLngLiteral>();
+  const [directions, setDirections] = useState<DirectionsResult>();
   const mapRef = useRef<GoogleMap>();
   const center = useMemo<LatLngLiteral>(
     () => ({ lat: 43.45, lng: -80.49 }),
@@ -23,14 +24,33 @@ export default function Map() {
 
   const options = useMemo<MapOptions>(
     () => ({
-      // mapId: "b181cac70f27f5e6", // optional
+      mapId: "b181cac70f27f5e6", // optional
       disableDefaultUI: true,
-      clickableIcons: false,
+      clickableIcons: true,
     }),
     []
   );
 
   const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const houses = useMemo(() => generateHouses(center), [center]);
+
+  const fetchDirections = (house: LatLngLiteral) => {
+    if (!office) return;
+
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: house,
+        destination: office,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === "OK" && result) {
+          setDirections(result);
+        }
+      }
+    );
+  };
 
   return (
     <div className="container">
@@ -42,8 +62,8 @@ export default function Map() {
             mapRef.current?.panTo(position);
           }}
         />
-
         {!office && <p>Enter the address of your office.</p>}
+        {directions && <Distance leg={directions.routes[0].legs[0]} />}
       </div>
 
       <div className="map">
@@ -54,14 +74,90 @@ export default function Map() {
           options={options}
           onLoad={onLoad}
         >
-          {office && (
-            <Marker
-              position={office}
-              icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+          {directions && (
+            <DirectionsRenderer
+              directions={directions}
+              options={{
+                polylineOptions: {
+                  zIndex: 50,
+                  strokeColor: "#1976D2",
+                  strokeWeight: 5,
+                },
+              }}
             />
+          )}
+
+          {office && (
+            <>
+              <Marker
+                position={office}
+                icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+              />
+
+              <MarkerClusterer>
+                {(clusterer) =>
+                  houses.map((house) => (
+                    <Marker
+                      key={`${house.lat}-${house.lng}`}
+                      position={house}
+                      clusterer={clusterer}
+                      onClick={() => {
+                        fetchDirections(house);
+                      }}
+                    />
+                  ))
+                }
+              </MarkerClusterer>
+
+              <Circle center={office} radius={10000} options={closeOptions} />
+              <Circle center={office} radius={13000} options={middleOptions} />
+              <Circle center={office} radius={16000} options={farOptions} />
+            </>
           )}
         </GoogleMap>
       </div>
     </div>
   );
 }
+
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+const closeOptions = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: "#8BC34A",
+  fillColor: "#8BC34A",
+};
+const middleOptions = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: "#FBC02D",
+  fillColor: "#FBC02D",
+};
+const farOptions = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: "#FF5252",
+  fillColor: "#FF5252",
+};
+
+const generateHouses = (position: LatLngLiteral) => {
+  const _houses: Array<LatLngLiteral> = [];
+  for (let i = 0; i < 100; i++) {
+    const direction = Math.random() < 0.5 ? -2 : 2;
+    _houses.push({
+      lat: position.lat + Math.random() / direction,
+      lng: position.lng + Math.random() / direction,
+    });
+  }
+  return _houses;
+};
