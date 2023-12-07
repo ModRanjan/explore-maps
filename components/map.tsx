1b03aae0d64969a9b521eba269a34ab7b8540a77
mod ranjan <modranjan1999@gmail.com>
2023-12-07 06:27:40 +0530

    set-up basic map

---
+++ components/map.tsx
@@ -0,0 +1,51 @@
+import { useState, useMemo, useCallback, useRef } from "react";
+import {
+  GoogleMap,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
+import Places from "./places";
+import Distance from "./distance";
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type DirectionsResult = google.maps.DirectionsResult;
+type MapOptions = google.maps.MapOptions;
+
+export default function Map() {
+  const mapRef = useRef<GoogleMap>();
+  const center = useMemo<LatLngLiteral>(
+    () => ({ lat: 43.45, lng: -80.49 }),
+    []
+  );
+
+  const options = useMemo<MapOptions>(
+    () => ({
+      // mapId: "b181cac70f27f5e6", // optional
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+
+  return (
+    <div className="container">
+      <div className="controls">
+        <h1>Commute?</h1>
+      </div>
+
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        ></GoogleMap>
+      </div>
+    </div>
+  );
+}
